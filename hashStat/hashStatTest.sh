57385e7dfca732a2957de1d3dc93a79409415cd1
zangtw <zangtw@gmail.com>
2015-07-08 17:49:19 -0500

    improve install scripts

--- hashStat/hashStatTest.sh
+++ hashStat/hashStatTest.sh
@@ -6,5 +6,5 @@ if [ "$1" == "--debug" ]; then
   export CFLAGS="-g3 -O0"
 fi
 
-$CXX $CFLAGS -dynamiclib -Wall -Werror -o libhashStat.dylib hashStat.c
+$CXX $CFLAGS -dynamiclib -Wall -Werror -install_name @rpath/libhashStat.dylib -o libhashStat.dylib hashStat.c
 $CXX $CFLAGS hashStatTest.c -o hashStatTest -L ./ -I ./ -l hashStat -Wl,-rpath,.
