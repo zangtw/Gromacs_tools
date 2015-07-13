a1e9d1279bc782468bddb29d629e5e073d028d71
zangtw <zangtw@gmail.com>
2015-07-12 23:23:14 -0500

    add multi-platform build

--- hashStat/hashStatTest.sh
+++ hashStat/hashStatTest.sh
@@ -1,10 +1,20 @@
 #/bin/bash
 
-export CXX=gcc
+export CC=gcc
 
 if [ "$1" == "--debug" ]; then
   export CFLAGS="-g3 -O0"
 fi
 
-$CXX $CFLAGS -dynamiclib -Wall -Werror -install_name @rpath/libhashStat.dylib -o libhashStat.dylib hashStat.c
-$CXX $CFLAGS hashStatTest.c -o hashStatTest -L ./ -I ./ -l hashStat -Wl,-rpath,.
+if [ "$(uname)" == 'Linux' ]; then
+  $CC $CFLAGS -c -Wall -Werror -fpic hashStat.c
+  $CC $CFLAGS -shared -o libhashStat.so hashStat.o
+  rm hashStat.o
+elif [ "$(uname)" == 'Darwin' ]; then
+  $CC $CFLAGS -dynamiclib -Wall -Werror -install_name @rpath/libhashStat.dylib -o libhashStat.dylib hashStat.c
+else
+	echo platform not supported!
+  exit 1
+fi
+
+$CC $CFLAGS hashStatTest.c -o hashStatTest -L ./ -I ./ -l hashStat -l m -Wl,-rpath,.
